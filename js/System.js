0224dbdba0ba2cdd48e37622b728713e0bfb25ee
SeaSnake10 <76809064+SeaSnake10@users.noreply.github.com>
2021-04-24 09:41:35 +0800

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,26 @@
+class System{
+
+    constructor(){}
+
+    // Add code to authenticate the given code and the access codes.
+    authenticate(actualCode,enteredCode){
+        if(actualCode === enteredCode)
+            return true
+        else
+            return false
+    }
+    authenticate(actualCode,enteredCode){
+        if(actualCode === enteredCode)
+            return true
+        else
+            return false
+    }
+    authenticate(actualCode,enteredCode){
+        if(actualCode === enteredCode)
+            return true
+        else
+            return false
+    }
+    
+
+}
